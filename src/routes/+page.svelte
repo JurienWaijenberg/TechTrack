b6f792e5e5d0779029863253624a05262eb0f8ac
JurienWaijenberg <j.waijenberg@gmail.com>
2025-11-11 12:37:15 +0100

    added all circuits of 2025

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Header from './header.svelte';
+    
+    let raceSessions: any[] = $state([]);
+
+    async function fetchData() {
+      const url = 'https://api.openf1.org/v1/sessions?year=2025&session_name=Race';
+
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log('2025 F1 Race Sessions:', result);
+        console.log(`Total race sessions found: ${result.length}`);
+        raceSessions = result;
+      }
+      catch(error: any) {
+        console.error('Error fetching sessions:', error.message);
+      }
+    }
+
+    $effect(() => {
+        if (raceSessions.length === 0) {
+            fetchData();
+        }
+    });
+</script>
+<Header />  
+
+<section class="race-sessions">
+    <h2>2025 F1 Race Sessions</h2>
+    {#if raceSessions.length > 0}
+        <p>Total race sessions found: {raceSessions.length}</p>
+            {#each raceSessions as session}
+                <section class="race-session">
+                    <h3>{session.circuit_short_name || JSON.stringify(session)}</h3>
+                    <p>{session.country_name || JSON.stringify(session)}</p>
+                </section>
+            {/each}
+    {:else}
+        <p>No race sessions found</p>
+    {/if}
+</section>
